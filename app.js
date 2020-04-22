20519f5f4795eda4e3edcec080a4548db900e5d1
Tonyobyo <michael.mastrucci@gmail.com>
2020-04-22 06:50:30 -0400

    2 Algorithms
    Added:
    - Factorialize a number
    - Convert C to F

---
+++ app.js
@@ -0,0 +1,21 @@
+//Factorialize a number
+function factorial(num) {
+  if(num < 0) {
+    return -1;
+  } else if(num == 0) {
+    return 1;
+  } else {
+    return (num * factorial (num - 1));
+  }
+}
+
+factorial(5);
+
+//Convert Celsius to Fahrenheit
+function tempConvert(celsius) {
+  let fahrenheit = celsius * (9/5) + 32;
+  return fahrenheit;
+}
+
+tempConvert(30);
+
